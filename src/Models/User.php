8bcaaba3a2eb01ad8b41f7cfbd36409a6c37d222
Chris Dimas <github@indie.systems>
2023-03-15 02:23:18 +0200

    User model comply with psr4 autoloading

--- src/Models/User.php
+++ src/Models/User.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace IndieSystems\PermissionsAdminlteUi;
+namespace IndieSystems\PermissionsAdminlteUi\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
